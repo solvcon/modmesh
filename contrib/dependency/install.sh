5afc35de7ea692d0bd8428836b1eadff77f6fe9f
Yung-Yu Chen <yyc@solvcon.net>
2020-11-14 19:49:29 +0800

    Update pybind11 to 2.6.1, not 2.6.2 (not released yet)

--- contrib/dependency/install.sh
+++ contrib/dependency/install.sh
@@ -44,8 +44,8 @@ pybind11() {
   cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
   cmakeargs+=("-DPYTHON_EXECUTABLE:FILEPATH=`which python3`")
   cmakeargs+=("-DPYBIND11_TEST=OFF")
-  install ${PYBIND_ORG:-pybind} pybind11 ${PYBIND_BRANCH:-v2.6.2} \
-    ${PYBIND_LOCAL:-pybind11-2.6.2} "${cmakeargs[@]}"
+  install ${PYBIND_ORG:-pybind} pybind11 ${PYBIND_BRANCH:-v2.6.1} \
+    ${PYBIND_LOCAL:-pybind11-2.6.1} "${cmakeargs[@]}"
 
 }
 
