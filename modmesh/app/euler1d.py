6139ca4a4d803414f5a996d40179c4b7796a5a9a
Yung-Yu Chen <yyc@solvcon.net>
2022-12-07 14:52:48 +0800

    Refactor the original 1D euler plotting controller
    Objective: enhance the euler1d app with more control
    
    * The original `Plotter` is renamed to `Controller`.
    * The code directly operate matplotlib is factored to a new `Plot` class.
    * A new data class `QuantityLine` is created to hold a pair of an analytical line and a numerical line for a quantity.

--- modmesh/app/euler1d.py
+++ modmesh/app/euler1d.py
@@ -25,8 +25,12 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 
+import sys
+from dataclasses import dataclass
+
 import numpy as np
 
+import matplotlib
 from matplotlib.backends.qt_compat import QtWidgets
 from matplotlib.backends.backend_qtagg import (
     FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
@@ -38,12 +42,60 @@ from ..onedim import euler1d
 
 
 def load_app():
-    cmd = "plt = mm.app.euler1d.run(animate=True, interval=10, max_steps=50)"
-    cmd += "; plt.start()"
+    view.app.pycon.writeToHistory("""
+# Use the functions for more examples:
+ctrl.start()  # Start the movie
+ctrl.step()  # Stepping the solution
+""")
+    cmd = "ctrl = mm.app.euler1d.run(interval=10, max_steps=50)"
     view.app.pycon.command = cmd
 
 
-class Plotter:
+@dataclass
+class QuantityLine:
+    ana: matplotlib.lines.Line2D = None
+    num: matplotlib.lines.Line2D = None
+    name: str = ""
+
+    def update(self, adata, ndata):
+        self.ana.set_ydata(adata.copy())
+        self.num.set_ydata(ndata.copy())
+        self.ana.figure.canvas.draw()
+        self.num.figure.canvas.draw()
+
+
+class Plot:
+    def __init__(self, figsize=(15, 10)):
+        self.fig = Figure(figsize=figsize)
+        self.canvas = FigureCanvas(self.fig)
+        self.ax = self.canvas.figure.subplots()
+        self.ax.set_ylim(-0.2, 1.2)
+        self.ax.grid()
+
+        self.density = QuantityLine(name="density")
+        self.velocity = QuantityLine(name="velocity")
+        self.pressure = QuantityLine(name="pressure")
+
+    def build_lines(self, x):
+        self.ax.set_xlim(x[0], x[-1])
+        for data, color in (
+                (self.density, 'r'),
+                (self.velocity, 'g'),
+                (self.pressure, 'b'),
+        ):
+            data.ana, = self.ax.plot(x.copy(), np.zeros_like(x), f'{color}-')
+            data.num, = self.ax.plot(x.copy(), np.zeros_like(x), f'{color}x')
+
+    def update_lines(self, shocktube):
+        self.density.update(adata=shocktube.density_field,
+                            ndata=shocktube.svr.density[::2])
+        self.pressure.update(adata=shocktube.pressure_field,
+                             ndata=shocktube.svr.pressure[::2])
+        self.velocity.update(adata=shocktube.velocity_field,
+                             ndata=shocktube.svr.velocity[::2])
+
+
+class Controller:
     def __init__(self, shocktube, max_steps, use_sub=None):
         if None is shocktube.gamma:
             raise ValueError("shocktube does not have constant built")
@@ -55,29 +107,27 @@ class Plotter:
         super().__init__()
         self._main = QtWidgets.QWidget()
         if self.use_sub:
+            # FIXME: sub window causes missing QWindow with the following error:
+            # RuntimeError: Internal C++ object (PySide6.QtGui.QWindow) already deleted.
+            # It is probably because RMainWindow is not recognized by PySide6
+            # and matplotlib.  We may consider to use composite for QMainWindow
+            # instead of inheritance.
             self._subwin = view.app.mainWindow.addSubWindow(self._main)
             self._subwin.resize(400, 300)
-        layout = QtWidgets.QVBoxLayout(self._main)
 
-        self.canvas = FigureCanvas(Figure(figsize=(15, 10)))
+        self.plt = Plot(figsize=(15, 10))
         # Ideally one would use self.addToolBar here, but it is slightly
         # incompatible between PyQt6 and other bindings, so we just add the
         # toolbar as a plain widget instead.
-        layout.addWidget(self.canvas)
-        layout.addWidget(NavigationToolbar(self.canvas, self._main))
+        layout = QtWidgets.QVBoxLayout(self._main)
+        layout.addWidget(self.plt.canvas)
+        layout.addWidget(NavigationToolbar(self.plt.canvas, self._main))
 
         self.max_steps = max_steps
-        self.step = 0
+        self.current_step = 0
+        self.timer = None
 
         self.shocktube = shocktube
-        self.ax = self.canvas.figure.subplots()
-        self.timer = None
-        self.density_num = None
-        self.velocity_num = None
-        self.pressure_num = None
-        self.density_ana = None
-        self.velocity_ana = None
-        self.pressure_ana = None
 
     def show(self):
         self._main.show()
@@ -90,68 +140,49 @@ class Plotter:
     def stop(self):
         self.timer.stop()
 
+    def step(self, steps=1):
+        self.march_alpha2(steps=steps)
+        if self.max_steps and self.current_step > self.max_steps:
+            self.stop()
+
     def march_alpha2(self, steps=1):
         self.shocktube.svr.march_alpha2(steps=steps)
-        self.step += steps
-        time_current = self.step * self.shocktube.svr.time_increment
+        self.current_step += steps
+        time_current = self.current_step * self.shocktube.svr.time_increment
         self.shocktube.build_field(t=time_current)
         cfl = self.shocktube.svr.cfl
-        print("CFL:", "min", cfl.min(), "max", cfl.max())
-        self._update_canvas()
-        if self.max_steps and self.step > self.max_steps:
-            self.stop()
+        self.log(f"CFL: min f{cfl.min()} max f{cfl.max()}")
+        self.plt.update_lines(self.shocktube)
 
     def setup_solver(self, interval):
         """
         :param interval: milliseconds
         :return: nothing
         """
-        st = self.shocktube
-        st.build_field(t=0)
+        self.shocktube.build_field(t=0)
         svr = self.shocktube.svr
-        x = svr.coord[::2]
-        self.ax.grid()
-        self.density_ana, = self.ax.plot(x, st.density_field, 'r-')
-        self.velocity_ana, = self.ax.plot(x, st.velocity_field, 'g-')
-        self.pressure_ana, = self.ax.plot(x, st.pressure_field, 'b-')
-        self.density_num, = self.ax.plot(x, svr.density[::2], 'rx')
-        self.velocity_num, = self.ax.plot(x, svr.velocity[::2], 'gx')
-        self.pressure_num, = self.ax.plot(x, svr.pressure[::2], 'bx')
-        self.timer = self.canvas.new_timer(interval)
-        self.timer.add_callback(self.march_alpha2)
+        self.plt.build_lines(svr.coord[::2])
+        self.timer = self.plt.canvas.new_timer(interval)
+        self.timer.add_callback(self.step)
 
-    def _update_canvas(self):
-        x = self.shocktube.svr.coord[::2]
-        self.density_ana.set_data(x, self.shocktube.density_field)
-        self.velocity_ana.set_data(x, self.shocktube.velocity_field)
-        self.pressure_ana.set_data(x, self.shocktube.pressure_field)
-        self.density_num.set_data(x, self.shocktube.svr.density[::2])
-        self.velocity_num.set_data(x, self.shocktube.svr.velocity[::2])
-        self.pressure_num.set_data(x, self.shocktube.svr.pressure[::2])
-        self.density_ana.figure.canvas.draw()
-        self.velocity_ana.figure.canvas.draw()
-        self.pressure_ana.figure.canvas.draw()
-        self.density_num.figure.canvas.draw()
-        self.velocity_num.figure.canvas.draw()
-        self.pressure_num.figure.canvas.draw()
+    @staticmethod
+    def log(msg):
+        sys.stdout.write(msg)
+        sys.stdout.write('\n')
+        view.app.pycon.writeToHistory(msg)
+        view.app.pycon.writeToHistory('\n')
 
 
-def run(animate, interval=10, max_steps=50):
+def run(interval=10, max_steps=50):
     st = euler1d.ShockTube()
     st.build_constant(gamma=1.4, pressure1=1.0, density1=1.0, pressure5=0.1,
                       density5=0.125)
     st.build_numerical(xmin=-10, xmax=10, ncoord=201, time_increment=0.05)
 
-    plt = Plotter(shocktube=st, max_steps=max_steps)
-    plt.show()
+    ctrl = Controller(shocktube=st, max_steps=max_steps)
+    ctrl.setup_solver(interval)
+    ctrl.show()
 
-    if animate:
-        plt.setup_solver(interval)
-    else:
-        plt.ax.plot(st.svr.xctr() / np.pi, st.svr.get_so0(0), '-')
-        st.svr.march_alpha2(50)
-        plt.ax.plot(st.svr.xctr() / np.pi, st.svr.get_so0(0), '+')
-
-    return plt
+    return ctrl
 
 # vim: set ff=unix fenc=utf8 et sw=4 ts=4 sts=4:
