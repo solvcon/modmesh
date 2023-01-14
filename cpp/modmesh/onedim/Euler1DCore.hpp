babbeb1b4f6a1ea5e80d1aa3196c2bd42fe3f1ca
Taihsiang Ho (tai271828) <e@tai271828.me>
2023-01-14 01:34:57 +0100

    feat(profiling): profile Euler1DKernel::derive

--- cpp/modmesh/onedim/Euler1DCore.hpp
+++ cpp/modmesh/onedim/Euler1DCore.hpp
@@ -32,6 +32,7 @@
 #include <modmesh/base.hpp>
 #include <modmesh/math.hpp>
 #include <modmesh/buffer/buffer.hpp>
+#include <modmesh/toggle/profile.hpp>
 #include <memory>
 
 namespace modmesh
@@ -182,6 +183,8 @@ struct Euler1DKernel
 
     Euler1DKernel & derive()
     {
+        MODMESH_TIME("Euler1DKernel::derive");
+
         // TODO: reduce numerical calculation.
         jac[0][0] = 0.0;
         jac[0][1] = 1.0;
