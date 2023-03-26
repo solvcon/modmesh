478792b9f36511cb882b32bb3ead63d2281ab32a
EN-Chou <dodger25685@gmail.com>
2023-03-26 17:04:48 +0800

    rm cavity2d_noQt from the list

--- cpp/modmesh/view/RManager.cpp
+++ cpp/modmesh/view/RManager.cpp
@@ -184,7 +184,6 @@ void RManager::setUpMenu()
         this->addApplication(QString("euler1d"));
         this->addApplication(QString("linear_wave"));
         this->addApplication(QString("bad_euler1d"));
-        this->addApplication(QString("cavity2d_noQt"));
         this->addApplication(QString("cavity2d"));
     }
 }
