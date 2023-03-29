462de8ab433b0f78aef78bfbd88686deb8332db8
EN-Chou <dodger25685@gmail.com>
2023-03-30 00:15:22 +0800

    create classes to classify values and grid system
    classify var and grid

--- modmesh/app/cavity2d.py
+++ modmesh/app/cavity2d.py
@@ -26,52 +26,70 @@ ctrl = mm.app.cavity2d.run(interval=10, max_steps=50, profiling=True)
 
 
 ###############################################################################
-N = 81
-dt = 0.001
-tol_p = 1e2
-tol_v = 1e-10
-Re = 100
-dx = 1.0 / (N - 1)
-dy = 1.0 / (N - 1)
 
-P = np.zeros((N + 1, N + 1))
-U = np.zeros((N + 1, N + 1))
-V = np.zeros((N + 1, N + 1))
-U_1 = np.zeros((N + 1, N + 1))
-V_1 = np.zeros((N + 1, N + 1))
-U_2 = np.zeros((N + 1, N + 1))
-V_2 = np.zeros((N + 1, N + 1))
-U_prev = np.zeros((N + 1, N + 1))
-V_prev = np.zeros((N + 1, N + 1))
-P_prev = np.zeros((N + 1, N + 1))
-UC = np.zeros((N, N))
-VC = np.zeros((N, N))
-PC = np.zeros((N, N))
+class var:
+    N = 32
+    dt = 0.001
+    tol_p = 1e2
+    tol_v = 1e-10
+    Re = 100
+
+class grid:
+    N = var.N
+    dx = 1.0 / (N - 1)
+    dy = 1.0 / (N - 1)
+    P = np.zeros((N + 1, N + 1))
+    U = np.zeros((N + 1, N + 1))
+    V = np.zeros((N + 1, N + 1))
+    U_1 = np.zeros((N + 1, N + 1))
+    V_1 = np.zeros((N + 1, N + 1))
+    U_2 = np.zeros((N + 1, N + 1))
+    V_2 = np.zeros((N + 1, N + 1))
+
+class prev:
+    N = grid.N
+    U = np.zeros((N + 1, N + 1))
+    V = np.zeros((N + 1, N + 1))
+    P = np.zeros((N + 1, N + 1))
+
+class col:
+    N = grid.N
+    U = np.zeros((N, N))
+    V = np.zeros((N, N))
+    P = np.zeros((N, N))
+
 
 # benchmark
-x_b = [0, 0.0625, 0.0703, 0.0781, 0.0938, 0.1563, 0.2266, 0.2344, 0.5000, 0.8047, 0.8594, 0.9063, 0.9453, 0.9531, 0.9609, 0.9688, 1.0]  # noqa: E501
-y_b = [0, 0.0547, 0.0625, 0.0703, 0.1016, 0.1719, 0.2813, 0.4531, 0.5000, 0.6172, 0.7344, 0.8516, 0.9531, 0.9609, 0.9688, 0.9766, 1.0]  # noqa: E501
-Re_100_v = [0, 0.09233, 0.10091, 0.10890, 0.12317, 0.16077, 0.17507, 0.17527, 0.05454, -0.24533, -0.22445, -0.16914, -0.10313, -0.08864, -0.07391, -0.05906, 0]  # noqa: E501
-Re_100_u = [0, -0.03717, -0.04192, -0.04775, -0.06434, -0.10150, -0.15662, -0.21090, -0.20581, -0.13641, 0.00332, 0.23151, 0.68717, 0.73722, 0.78871, 0.84123, 1]  # noqa: E501
-
-pt = np.zeros(N)
-for i in range(1, N):
-    pt[i] = (i - 1) / (N - 1)
+class benchmark:
+    N = var.N
+    x_b = [0, 0.0625, 0.0703, 0.0781, 0.0938, 0.1563, 0.2266, 0.2344, 0.5000]
+    x_b = x_b + [0.8047, 0.8594, 0.9063, 0.9453, 0.9531, 0.9609, 0.9688, 1.0]
+    y_b = [0, 0.0547, 0.0625, 0.0703, 0.1016, 0.1719, 0.2813, 0.4531, 0.5000]
+    y_b = y_b + [0.6172, 0.7344, 0.8516, 0.9531, 0.9609, 0.9688, 0.9766, 1.0]
+    Re_100_v = [0, 0.09233, 0.10091, 0.10890, 0.12317, 0.16077, 0.17507]
+    Re_100_v = Re_100_v + [0.17527, 0.05454, -0.24533, -0.22445, -0.16914]
+    Re_100_v = Re_100_v + [-0.10313, -0.08864, -0.07391, -0.05906, 0]
+    Re_100_u = [0, -0.03717, -0.04192, -0.04775, -0.06434, -0.10150, -0.15662]
+    Re_100_u = Re_100_u + [-0.21090, -0.20581, -0.13641, 0.00332, 0.23151]
+    Re_100_u = Re_100_u + [0.68717, 0.73722, 0.78871, 0.84123, 1]
+    pt = np.zeros(N)
+    for i in range(1, N):
+        pt[i] = (i - 1) / (N - 1)
 
 
 def meet_poisson():
     residual = 0
-    for i in range(1, N):
-        for j in range(1, N):
-            ux = (U_2[i, j] - U_2[i - 1, j]) / dx
-            vy = (V_2[i, j] - V_2[i, j - 1]) / dy
-            poisson_LHP = (ux + vy) / dt
-            poisson_RHP = (P[i + 1, j] + P[i - 1, j] + P[i, j + 1] + P[i, j - 1] - 4 * P[i, j]) / (dx * dx)  # noqa: E501
+    for i in range(1, grid.N):
+        for j in range(1, grid.N):
+            ux = (grid.U_2[i, j] - grid.U_2[i - 1, j]) / grid.dx
+            vy = (grid.V_2[i, j] - grid.V_2[i, j - 1]) / grid.dy
+            poisson_LHP = (ux + vy) / var.dt
+            poisson_RHP = (grid.P[i + 1, j] + grid.P[i - 1, j] + grid.P[i, j + 1] + grid.P[i, j - 1] - 4 * grid.P[i, j]) / (grid.dx * grid.dx)  # noqa: E501
             residual += abs(poisson_LHP - poisson_RHP)
 
     # clear_output(wait=True)
     log(f"[meet Poisson]residual:  {residual}")
-    if residual < tol_p:
+    if residual <  var.tol_p:
         return True
 
     return False
@@ -86,129 +104,134 @@ def log(msg):
 
 def is_steady():
     vt = 0
-    for i in range(1, N):
-        for j in range(1, N):
-            vt += abs(U_prev[i, j] - U[i, j])
-            vt += abs(V_prev[i, j] - V[i, j])
-            U_prev[i, j] = U[i, j]
-            V_prev[i, j] = V[i, j]
+    for i in range(1, grid.N):
+        for j in range(1, grid.N):
+            vt += abs(prev.U[i, j] - grid.U[i, j])
+            vt += abs(prev.V[i, j] - grid.V[i, j])
+            prev.U[i, j] = grid.U[i, j]
+            prev.V[i, j] = grid.V[i, j]
 
     log(f"[is_steady] velocity deviation: {vt}")
 
-    if vt < tol_v:
+    if vt <  var.tol_v:
         return True
 
     return False
 
 
 def collocate():
-    for i in range(N):
-        for j in range(N):
-            UC[i, j] = 0.5 * (U[i, j] + U[i, j + 1])
-            VC[i, j] = 0.5 * (V[i, j] + V[i + 1, j])
-            PC[i, j] = (P[i, j] + P[i + 1, j] + P[i, j + 1] + P[i + 1, j + 1]) * 0.25  # noqa: E501
+    for i in range(grid.N):
+        for j in range(grid.N):
+            col.U[i, j] = 0.5 * (grid.U[i, j] + grid.U[i, j + 1])
+            col.V[i, j] = 0.5 * (grid.V[i, j] + grid.V[i + 1, j])
+            col.P[i, j] = (grid.P[i, j] + grid.P[i + 1, j] + grid.P[i, j + 1] + grid.P[i + 1, j + 1]) * 0.25  # noqa: E501
 
 
 def moniter(timestep):
     #   clear_output(wait=True)
+    bm = benchmark()
     print("Timestep: ", timestep)
     fig, ax = plt.subplots(1, 3, figsize=(17, 5))
-    ax[0].plot(Re_100_u, y_b, 'bo', np.transpose(UC)[:, int((N + 1) / 2)], pt)
+    U_mid = np.transpose(col.U)[:, int((grid.N + 1) / 2)]
+    V_mid = np.transpose(col.V)[int((grid.N + 1) / 2)]
+    velocity = np.transpose(np.sqrt(np.multiply(col.U, col.U) + np.multiply(col.V, col.V)))
+    ax[0].plot(bm.Re_100_u, bm.y_b, 'bo', U_mid, bm.pt)
     ax[0].set(ylim=(0, 1))
-    ax[1].plot(x_b, Re_100_v, 'bo', pt, np.transpose(VC)[int((N + 1) / 2)])
+    ax[1].plot(bm.x_b, bm.Re_100_v, 'bo', bm.pt, V_mid)
     ax[1].set(xlim=(0, 1))
-    ax[2].imshow(np.transpose(np.sqrt(np.multiply(UC, UC) + np.multiply(VC, VC))))  # noqa: E501
+    ax[2].imshow(velocity)
     ax[2].invert_yaxis()
     plt.show()
 
 
 # Computation-1
 def setBC(option):
+    N=grid.N
     if option == 'P':
-        P[0, :] = P[1, :]  # west
-        P[N, :] = P[N - 1, :]  # east
-        P[:, 0] = P[:, 1]  # south
-        P[:, N] = P[:, N - 1]  # north
+        grid.P[0, :] = grid.P[1, :]  # west
+        grid.P[N, :] = grid.P[N - 1, :]  # east
+        grid.P[:, 0] = grid.P[:, 1]  # south
+        grid.P[:, N] = grid.P[:, N - 1]  # north
 
     elif option == 'U':
-        U[:, N] = 2 - U[:, N - 1]  # north
-        U[:, 0] = - U[:, 1]  # south
-        U[0, :] = 0  # west
-        U[N - 1, :] = 0  # east
+        grid.U[:, N] = 2 - grid.U[:, N - 1]  # north
+        grid.U[:, 0] = - grid.U[:, 1]  # south
+        grid.U[0, :] = 0  # west
+        grid.U[N - 1, :] = 0  # east
 
-        U_1[:, N] = 2 - U_1[:, N - 1]  # north
-        U_1[:, 0] = - U_1[:, 1]  # south
-        U_1[0, :] = 0  # west
-        U_1[N-1, :] = 0  # east
+        grid.U_1[:, N] = 2 - grid.U_1[:, N - 1]  # north
+        grid.U_1[:, 0] = - grid.U_1[:, 1]  # south
+        grid.U_1[0, :] = 0  # west
+        grid.U_1[N-1, :] = 0  # east
 
-        U_2[:, N] = 2 - U_2[:, N - 1]  # north
-        U_2[:, 0] = - U_2[:, 1]  # south
-        U_2[0, :] = 0  # west
-        U_2[N - 1, :] = 0  # east
+        grid.U_2[:, N] = 2 - grid.U_2[:, N - 1]  # north
+        grid.U_2[:, 0] = - grid.U_2[:, 1]  # south
+        grid.U_2[0, :] = 0  # west
+        grid.U_2[N - 1, :] = 0  # east
 
     elif option == 'V':
-        V[0, :] = - V[1, :]  # west
-        V[N, :] = - V[N - 1, :]  # east
-        V[:, 0] = 0  # south
-        V[:, N - 1] = 0  # north
+        grid.V[0, :] = - grid.V[1, :]  # west
+        grid.V[N, :] = - grid.V[N - 1, :]  # east
+        grid.V[:, 0] = 0  # south
+        grid.V[:, N - 1] = 0  # north
 
-        V_1[0, :] = - V_1[1, :]  # west
-        V_1[N, :] = - V_1[N - 1, :]  # east
-        V_1[:, 0] = 0  # south
-        V_1[:, N - 1] = 0  # north
+        grid.V_1[0, :] = - grid.V_1[1, :]  # west
+        grid.V_1[N, :] = - grid.V_1[N - 1, :]  # east
+        grid.V_1[:, 0] = 0  # south
+        grid.V_1[:, N - 1] = 0  # north
 
-        V_2[0, :] = - V_2[1, :]  # west
-        V_2[N, :] = - V_2[N - 1, :]  # east
-        V_2[:, 0] = 0  # south
-        V_2[:, N-1] = 0  # north
+        grid.V_2[0, :] = - grid.V_2[1, :]  # west
+        grid.V_2[N, :] = - grid.V_2[N - 1, :]  # east
+        grid.V_2[:, 0] = 0  # south
+        grid.V_2[:, N-1] = 0  # north
 
 
 def solve_U1():
-    for i in range(1, N - 1):
-        for j in range(1, N):
-            u = U[i, j]
-            v = (V[i, j] + V[i + 1, j] + V[i, j - 1] + V[i + 1, j - 1]) / 4
-            ux = (U[i + 1, j] - U[i - 1, j]) / (2 * dx)
-            uy = (U[i, j + 1] - U[i, j - 1]) / (2 * dy)
-            u2x = (U[i + 1, j] + U[i - 1, j] - 2 * U[i, j]) / (dx * dx)
-            u2y = (U[i, j + 1] + U[i, j - 1] - 2 * U[i, j]) / (dy * dy)
+    for i in range(1, grid.N - 1):
+        for j in range(1, grid.N):
+            u = grid.U[i, j]
+            v = (grid.V[i, j] + grid.V[i + 1, j] + grid.V[i, j - 1] + grid.V[i + 1, j - 1]) / 4
+            ux = (grid.U[i + 1, j] - grid.U[i - 1, j]) / (2 * grid.dx)
+            uy = (grid.U[i, j + 1] - grid.U[i, j - 1]) / (2 * grid.dy)
+            u2x = (grid.U[i + 1, j] + grid.U[i - 1, j] - 2 * grid.U[i, j]) / (grid.dx * grid.dx)
+            u2y = (grid.U[i, j + 1] + grid.U[i, j - 1] - 2 * grid.U[i, j]) / (grid.dy * grid.dy)
 
             C = u * ux + v * uy
-            D = (u2x + u2y) / Re
+            D = (u2x + u2y) / var.Re
 
-            px = (P[i + 1, j] - P[i, j]) / dx
-            U_1[i, j] = (- C + D - px) * dt + U[i, j]
+            px = (grid.P[i + 1, j] - grid.P[i, j]) / grid.dx
+            grid.U_1[i, j] = (- C + D - px) * var.dt + grid.U[i, j]
 
 
 def solve_V1():
-    for i in range(1, N):
-        for j in range(1, N - 1):
-            u = (U[i - 1, j + 1] + U[i, j + 1] + U[i - 1, j] + U[i, j]) / 4.0
-            v = V[i, j]
-            vx = (V[i + 1, j] - V[i - 1, j]) / (2 * dx)
-            vy = (V[i, j + 1] - V[i, j - 1]) / (2 * dy)
-            v2x = (V[i + 1, j] + V[i - 1, j] - 2 * V[i, j]) / (dx * dx)
-            v2y = (V[i, j + 1] + V[i, j - 1] - 2 * V[i, j]) / (dy * dy)
+    for i in range(1, grid.N):
+        for j in range(1, grid.N - 1):
+            u = (grid.U[i - 1, j + 1] + grid.U[i, j + 1] + grid.U[i - 1, j] + grid.U[i, j]) / 4.0
+            v = grid.V[i, j]
+            vx = (grid.V[i + 1, j] - grid.V[i - 1, j]) / (2 * grid.dx)
+            vy = (grid.V[i, j + 1] - grid.V[i, j - 1]) / (2 * grid.dy)
+            v2x = (grid.V[i + 1, j] + grid.V[i - 1, j] - 2 * grid.V[i, j]) / (grid.dx * grid.dx)
+            v2y = (grid.V[i, j + 1] + grid.V[i, j - 1] - 2 * grid.V[i, j]) / (grid.dy * grid.dy)
 
             C = u * vx + v * vy
-            D = (v2x + v2y) / Re
+            D = (v2x + v2y) / var.Re
 
-            py = (P[i, j + 1] - P[i, j]) / dy
-            V_1[i][j] = (- C + D - py) * dt + V[i, j]
+            py = (grid.P[i, j + 1] - grid.P[i, j]) / grid.dy
+            grid.V_1[i][j] = (- C + D - py) * var.dt + grid.V[i, j]
 
 
 def solve_U2():
-    for i in range(1, N - 1):
-        for j in range(1, N):
-            px = (P[i + 1, j] - P[i, j]) / dx
-            U_2[i, j] = px * dt + U_1[i, j]
+    for i in range(1, grid.N - 1):
+        for j in range(1, grid.N):
+            px = (grid.P[i + 1, j] - grid.P[i, j]) / grid.dx
+            grid.U_2[i, j] = px * var.dt + grid.U_1[i, j]
 
 
 def solve_V2():
-    for i in range(1, N):
-        for j in range(1, N - 1):
-            py = (P[i, j + 1] - P[i, j]) / dy
-            V_2[i, j] = py * dt + V_1[i, j]
+    for i in range(1, grid.N):
+        for j in range(1, grid.N - 1):
+            py = (grid.P[i, j + 1] - grid.P[i, j]) / grid.dy
+            grid.V_2[i, j] = py * var.dt + grid.V_1[i, j]
 
 
 def solve_P():
@@ -217,27 +240,27 @@ def solve_P():
         iteration = iteration + 1
         # clear_output(wait=True)
         # print("[solve_P]iteration: ", iteration)
-        for i in range(1, N):
-            for j in range(1, N):
-                ux = (U_2[i, j] - U_2[i - 1, j]) / dx
-                vy = (V_2[i, j] - V_2[i, j - 1]) / dy
-                poisson_LHP = (ux + vy) / dt
-                P[i, j] = 0.25 * (P[i + 1, j] + P[i - 1, j] + P[i, j + 1] + P[i, j - 1] - poisson_LHP * dx * dx)  # noqa: E501
+        for i in range(1, grid.N):
+            for j in range(1, grid.N):
+                ux = (grid.U_2[i, j] - grid.U_2[i - 1, j]) / grid.dx
+                vy = (grid.V_2[i, j] - grid.V_2[i, j - 1]) / grid.dy
+                poisson_LHP = (ux + vy) / var.dt
+                grid.P[i, j] = 0.25 * (grid.P[i + 1, j] + grid.P[i - 1, j] + grid.P[i, j + 1] + grid.P[i, j - 1] - poisson_LHP * grid.dx * grid.dx)  # noqa: E501
     meet_poisson()
 
 
 def solve_U():
-    for i in range(1, N - 1):
-        for j in range(1, N):
-            px = (P[i + 1, j] - P[i, j]) / dx
-            U[i, j] = - px * dt + U_2[i, j]
+    for i in range(1, grid.N - 1):
+        for j in range(1, grid.N):
+            px = (grid.P[i + 1, j] - grid.P[i, j]) / grid.dx
+            grid.U[i, j] = - px * var.dt + grid.U_2[i, j]
 
 
 def solve_V():
-    for i in range(1, N):
-        for j in range(1, N-1):
-            py = (P[i, j+1] - P[i, j]) / dy
-            V[i, j] = - py * dt + V_2[i, j]
+    for i in range(1, grid.N):
+        for j in range(1, grid.N-1):
+            py = (grid.P[i, j+1] - grid.P[i, j]) / grid.dy
+            grid.V[i, j] = - py * var.dt + grid.V_2[i, j]
 
 
 # Computation-2
