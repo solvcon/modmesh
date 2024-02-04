a75024f867dec112e30abeaae247a6d86a82fb07
Terry Chan <terrychan999@gmail.com>
2024-02-05 00:27:11 +0800

    download metal-cpp by macOS version

--- thirdparty/metal-cpp.sh
+++ thirdparty/metal-cpp.sh
@@ -27,14 +27,35 @@ download_md5 () {
   fi
 }
 
-filename=metal-cpp_macOS12_iOS15.zip
+if [ $(uname) == Darwin ] ; then
+  ver=$(sw_vers -productVersion)
+  if [ ${ver:0:2} == "12" ] ; then
+    filename=metal-cpp_macOS12_iOS15.zip
+    url=https://developer.apple.com/metal/cpp/files/metal-cpp_macOS12_iOS15.zip
+    checksum=8faab6897ba1f62e87076f153e036a58
+  elif [ ${ver:0:2} == "13" ] ; then
+    filename=metal-cpp_macOS13_iOS16.zip
+    url=https://developer.apple.com/metal/cpp/files/metal-cpp_macOS13.3_iOS16.4.zip
+    checksum=771a496981fb79dbd11d8bb128f19158
+  elif [ ${ver:0:2} == "14" ] ; then
+    filename=metal-cpp_macOS14_iOS17.zip
+    url=https://developer.apple.com/metal/cpp/files/metal-cpp_macOS14.2_iOS17.2.zip
+    checksum=8ec6c894233c834f7c611c575be72315
+  else
+    echo "Unsupported macOS version $ver"
+    exit 1
+  fi
+else
+  echo "Unsupported OS $(uname)"
+  exit 1
+fi
 
 pushd ${script_root}
 
 download_md5 \
   ${script_root}/archive/${filename} \
-  https://developer.apple.com/metal/cpp/files/metal-cpp_macOS12_iOS15.zip \
-  8faab6897ba1f62e87076f153e036a58
+  ${url} \
+  ${checksum}
 
 unzip ${script_root}/archive/${filename} -d install -x __MACOSX/\*
 
