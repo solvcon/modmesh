f362f4e85476366854555903e85e2ea5d40a7eb1
ntut-xuan <t109590031@ntut.org.tw>
2025-07-26 02:25:21 +0800

    Implement: Make profiling as packaging

---
+++ modmesh/profiling/__main__.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2025, Han-Xuan Huang <c1ydehhx@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# - Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# - Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+import json
+
+if __name__ == "__main__":
+    from modmesh.profiling._profiling_result_printer import (
+        ProfilingResultPrinter,
+    )
+
+    if len(sys.argv) < 2:
+        print(
+            "Usage: python -m modmesh.profiling <profiling_result_file.json>"
+        )
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print(f"File not found: {filepath}")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print(f"Invalid JSON file: {filepath}")
+        sys.exit(1)
+
+    printer = ProfilingResultPrinter(data)
+
+    printer.add_column("total_time", lambda r: r.total_time)
+    printer.add_column("count", lambda r: r.count)
+
+    printer.print_result()
+
+# vim: set ff=unix fenc=utf8 et sw=4 ts=4 sts=4:
