181ec3ebe6ef3c84997004a3650bc3201a406b12
Yung-Yu Chen <yyc@solvcon.net>
2025-11-22 16:30:03 +0800

    Refactor: extract `Triangle3d`, `TrianglePad` into `modmesh/universe/polygon.hpp`

--- cpp/modmesh/universe/universe.hpp
+++ cpp/modmesh/universe/universe.hpp
@@ -31,6 +31,7 @@
 #include <modmesh/universe/bernstein.hpp>
 #include <modmesh/universe/bezier.hpp>
 #include <modmesh/universe/coord.hpp>
+#include <modmesh/universe/polygon.hpp>
 #include <modmesh/universe/World.hpp>
 
 namespace modmesh
