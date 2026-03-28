5d882206866315f1a63c6a613a384402af055a82
Yung-Yu Chen <yyc@solvcon.net>
2026-03-28 15:19:54 +0800

    Extract `WrapSegment3d` and `WrapSegmentPad` to a distinct file
    * Create a new file `wrap_shape1d.cpp` to house wrapping code for 1-D geometry
      entity.
    * Move the C++ wrapper classes `WrapSegment3d` and `WrapSegmentPad` from file
      `wrap_World.cpp` to `wrap_shape1d.cpp`.
    
    Claude code command:
    * Extract `WrapSegment3d` and `WrapSegmentPad` from `wrap_World.cpp` to a new
      file `wrap_shape1d.cpp`

--- cpp/modmesh/universe/pymod/universe_pymod.hpp
+++ cpp/modmesh/universe/pymod/universe_pymod.hpp
@@ -43,6 +43,7 @@ namespace python
 void initialize_universe(pybind11::module & mod);
 void wrap_bernstein(pybind11::module & mod);
 void wrap_shape0d(pybind11::module & mod);
+void wrap_shape1d(pybind11::module & mod);
 void wrap_World(pybind11::module & mod);
 void wrap_polygon(pybind11::module & mod);
 
