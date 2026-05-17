92ffe5c75ccb47a59ecd20d15ef1691fc88addd3
Zong-han, Xie <zonghanxie@proton.me>
2026-05-17 21:06:28 +0800

    Relate to issue-786
    1. Modify DataFrame code to accept os.PathLike csv paths.
    2. Add tests to explicitly test if dataframe can accept paths from
       `pathlib` as well as a string
    3. Enhance the exception raise to explicitly indicates the files are not
       found.
    4. Add tests to test raised Exception type as well as the error message.

--- tests/test_timeseries_dataframe.py
+++ tests/test_timeseries_dataframe.py
@@ -25,6 +25,9 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 import io
+import os
+import pathlib
+import tempfile
 import unittest
 
 import numpy as np
@@ -163,3 +166,39 @@ class TimeSeriesDataFrameTC(unittest.TestCase):
         col_data = reordered_tsdf['DELTA_VEL[1]']
         nd_arr = np.genfromtxt(io.StringIO(self.dlc_data), delimiter=',')[1:]
         self.assertEqual(list(col_data), list(nd_arr[:, 1]))
+
+    def test_read_from_text_file_accepts_str_path(self):
+        tsdf = dataframe.DataFrame()
+        with tempfile.NamedTemporaryFile(
+            mode='w', suffix='.csv', delete=False,
+        ) as fh:
+            fh.write(self.dlc_data)
+            path = fh.name
+        try:
+            tsdf.read_from_text_file(path)
+            self.assertEqual(tsdf._columns, self.col_sol)
+            self.assertEqual(tsdf._index_name, 'EPOCH')
+        finally:
+            os.unlink(path)
+
+    def test_read_from_text_file_accepts_pathlib_path(self):
+        tsdf = dataframe.DataFrame()
+        with tempfile.NamedTemporaryFile(
+            mode='w', suffix='.csv', delete=False,
+        ) as fh:
+            fh.write(self.dlc_data)
+            path = pathlib.Path(fh.name)
+        try:
+            tsdf.read_from_text_file(path)
+            self.assertEqual(tsdf._columns, self.col_sol)
+            self.assertEqual(tsdf._index_name, 'EPOCH')
+        finally:
+            path.unlink()
+
+    def test_read_from_text_file_missing_raises_filenotfound(self):
+        tsdf = dataframe.DataFrame()
+        missing = pathlib.Path(tempfile.gettempdir()) / 'no_such_file.csv'
+        expected_msg = "Text file '{}' does not exist".format(missing)
+        with self.assertRaises(FileNotFoundError) as ctx:
+            tsdf.read_from_text_file(missing)
+        self.assertEqual(str(ctx.exception), expected_msg)
